571f26f80038223c4519e86b601ea9489050b012
Ruagdo <83822127+Ruagdo@users.noreply.github.com>
2021-08-09 20:47:15 -0700

    SAAS Import Loaders -webpack.config.js RuthAguirre

--- proyectofinal/webpack.config.js
+++ proyectofinal/webpack.config.js
@@ -1,24 +1,23 @@
 //webpack.config.js
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [
-          "style-loader",
-          {
-            loader: "css-loader",
-            options: {
-              importLoaders: 2,
-              // 0 => no loaders (default);
-              // 1 => postcss-loader;
-              // 2 => postcss-loader, sass-loader
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            "style-loader",
+            "css-loader",
+            {
+              loader: "sass-loader",
+              options: {
+                implementation: require("sass"),
+                sassOptions: {
+                  fiber: require("fibers"),
+                },
+              },
             },
-          },
-          "postcss-loader",
-          "sass-loader",
-        ],
-      },
-    ],
-  },
-};
+          ],
+        },
+      ],
+    },
+  };
