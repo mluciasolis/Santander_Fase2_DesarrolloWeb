3616b4df426c13299ba919a0d8becb2e56df2a0b
Magda Lucia Hernandez Solis <magdalucia97@gmail.com>
2021-08-16 01:03:51 -0500

    Magda: Vista de resultados del input de busqueda

---
+++ proyectofinal/src/js/busqueda.js
@@ -0,0 +1,40 @@
+'use strict';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import '../css/styles.css';
+import { getRecetasBuscador } from './funciones.js';
+import { buscar } from './funciones.js';
+
+const carousel = document.querySelector('.carousel');
+const sectionRecetas = document.querySelector('.recetas');
+const recetas = document.querySelector('.listaRecetas');
+const botonBuscar = document.querySelector('.buscar');
+const inputBuscar = document.querySelector('.inputBuscar');
+
+let urlRecetasBusqueda = "";
+
+//Accedes a los parametros del URL
+const valores = window.location.search;
+//Creamos la instancia
+const urlParams = new URLSearchParams(valores);
+//Accedemos a los valores
+const busqueda = urlParams.get('i');
+//console.log(busqueda)
+
+document.querySelector(".tituloResultados").innerHTML = `Resultados de: ${busqueda}`;
+
+urlRecetasBusqueda = `https://www.themealdb.com/api/json/v1/1/filter.php?i=` + busqueda
+
+///Muestra las recetas en la página de búsqueda
+getRecetasBuscador(recetas, urlRecetasBusqueda)
+
+botonBuscar.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (inputBuscar.value) {
+        buscar(inputBuscar.value)
+    } else
+        console.log("no se buscó nada")
+})
+
+
+
