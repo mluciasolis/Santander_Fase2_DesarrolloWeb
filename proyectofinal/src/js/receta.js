7bd8e61183afd11471a700d4bc65dc7d267e81ed
Zaire <raffexrivaille@gmail.com>
2021-08-18 15:55:08 -0500

    Zaire: Receta.js y estructura html

--- proyectofinal/src/js/receta.js
+++ proyectofinal/src/js/receta.js
@@ -36,17 +36,18 @@ urlRecetaBusqueda = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=` + bu
 //getRecetasBuscador(recetas, urlRecetasBusqueda)
 /*getReceta(recetas,urlRecetaBusqueda)*/
 
+
 getImagesFromMeal(urlRecetaBusqueda)
         .then((data) => {
             data = data.meals[0]
             imgreceta.src = data.strMealThumb;
             document.querySelector(".Instruccion1").innerHTML = ` <svg class="bi bi-check-circle-fill" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#F24726" viewBox="0 0 16 16">
             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"></path>
-          </svg>   ${data.strInstructions}.`;        
-            document.querySelector(".Ingremg1").innerHTML = `${data.strIngredient1}`;
-            document.querySelector(".Ingremg2").innerHTML = `${data.strIngredient2}`;
-            document.querySelector(".Ingremg3").innerHTML = `${data.strIngredient3}`;
-            document.querySelector(".Ingremg4").innerHTML = `${data.strIngredient4}`;
+          </svg> ${data.strInstructions}`;        
+            document.querySelector(".Ingrediente1").innerHTML = `${data.strIngredient1}`;
+            document.querySelector(".Ingrediente2").innerHTML = `${data.strIngredient2}`;
+            document.querySelector(".Ingrediente3").innerHTML = `${data.strIngredient3}`;
+            document.querySelector(".Ingrediente4").innerHTML = `${data.strIngredient4}`;
 
             console.log(data)
         })
@@ -58,3 +59,6 @@ botonBuscar.addEventListener("click", (e) => {
     } else
         console.log("no se buscó nada")
 })
+
+
+
