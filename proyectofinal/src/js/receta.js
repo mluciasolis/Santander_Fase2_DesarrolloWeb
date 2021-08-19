6dd4948872cc019d7029b893eb18a8dbced99172
Zaire <raffexrivaille@gmail.com>
2021-08-18 20:19:27 -0500

    Zaire: Recetas js y html

--- proyectofinal/src/js/receta.js
+++ proyectofinal/src/js/receta.js
@@ -2,7 +2,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import '../css/styles.css';
-import { getRecetasBuscador,getReceta } from './funciones.js';
+import {getImagesFromMeal} from './funciones.js';
 import { buscar } from './funciones.js';
 
 // const carousel = document.querySelector('.carousel');
@@ -10,6 +10,8 @@ import { buscar } from './funciones.js';
 const recetas = document.querySelector('.listaRecetas');
 const botonBuscar = document.querySelector('.buscar');
 const inputBuscar = document.querySelector('.inputBuscar');
+
+
 //const receta = document.querySelector("#")
 
 let urlRecetaBusqueda = "";
@@ -22,13 +24,56 @@ const urlParams = new URLSearchParams(valores);
 const busqueda = urlParams.get('i');
 const nombre = urlParams.get('nombre');
 
-document.querySelector(".tituloResultados").innerHTML = `Resultados de: ${nombre}`;
+document.querySelector(".tituloReceta").innerHTML = `${nombre}`;
 
 urlRecetaBusqueda = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=` + busqueda
 
+//se instancia el url que se concatenará al nombre del ingrediente obtenido.
+const pre_url = `https://www.themealdb.com/images/ingredients/`
+
+//Para rellenar los html a travéss de querySelector
+const imgreceta = document.querySelector('.imgrecetita')
+const img_ing1 = document.querySelector('.img1-ingrediente')
+const img_ing2 = document.querySelector('.img2-ingrediente')
+const img_ing3 = document.querySelector('.img3-ingrediente')
+const img_ing4 = document.querySelector('.img4-ingrediente')
+
+let img_ingrediente1 = "";
+let img_ingrediente2 = "";
+let img_ingrediente3 = "";
+let img_ingrediente4 = "";
+
 ///Muestra las recetas en la página de búsqueda
 //getRecetasBuscador(recetas, urlRecetasBusqueda)
-getReceta(recetas,urlRecetaBusqueda)
+/*getReceta(recetas,urlRecetaBusqueda)*/
+
+
+getImagesFromMeal(urlRecetaBusqueda)
+        .then((data) => {
+            data = data.meals[0]
+            imgreceta.src = data.strMealThumb;
+            document.querySelector(".Instruccion1").innerHTML = ` <svg class="bi bi-check-circle-fill" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#F24726" viewBox="0 0 16 16">
+            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"></path>
+          </svg> ${data.strInstructions}`;    
+                   
+
+            img_ing1.src =  pre_url + `${data.strIngredient1}.png`;      
+            img_ing2.src = pre_url + `${data.strIngredient2}.png`;
+            img_ing3.src = pre_url + `${data.strIngredient3}.png`;
+            img_ing4.src = pre_url + `${data.strIngredient4}.png`;
+
+            document.querySelector(".Ingrediente1").innerHTML = `${data.strIngredient1}`;
+            document.querySelector(".Ingrediente2").innerHTML = `${data.strIngredient2}`;
+            document.querySelector(".Ingrediente3").innerHTML = `${data.strIngredient3}`;
+            document.querySelector(".Ingrediente4").innerHTML = `${data.strIngredient4}`;
+
+            document.querySelector(".medidas-ingrediente1").innerHTML = `${data.strMeasure1}`;
+            document.querySelector(".medidas-ingrediente2").innerHTML = `${data.strMeasure2}`;
+            document.querySelector(".medidas-ingrediente3").innerHTML = `${data.strMeasure3}`;
+            document.querySelector(".medidas-ingrediente4").innerHTML = `${data.strMeasure4}`;
+
+            console.log(data)
+        })
 
 botonBuscar.addEventListener("click", (e) => {
     e.preventDefault();
